199852917ca56631b84e01c0375618ec592fac92
Jin <jinparkhan+git@googlemail.com>
2017-01-01 19:11:44 +0000

    Before adding flask_WTF form input.  Currently very simple input method.

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 from logging import DEBUG
+from forms import BookmarkForm
+
 
 app = Flask(__name__)
 app.logger.setLevel(DEBUG)
@@ -14,6 +16,8 @@ class User:
         return "{}. {}.".format(self.firstname[0], self.lastname[0])
 
 bookmarks = []
+app.config['SECRET_KEY'] = b"2\xcf\xcc\xd1M'\xe9_a9u\x1c\xf65\xfa\x10/Ac\xf0\xc6\xc4q\x99"
+
 
 def store_bookmark(urlfromAddRoute):
     bookmarks.append(dict(
@@ -22,17 +26,24 @@ def store_bookmark(urlfromAddRoute):
         date = datetime.utcnow()
     ))
 
+#Book mark sorted by date
+def new_bookmarks(num):
+    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title="Title passed from view to template", user=User("Jin", "Park"))
+    return render_template('index.html', title="Title passed from view to template", user=User("Jin", "Park"), new_bookmarks=new_bookmarks(5))
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    form = BookmarkForm()
     if request.method == "POST":
+        #url = form.url.data
         urlInAddRoute = request.form['urlInAddHtml']
         store_bookmark(urlInAddRoute)
+        flash("Stored bookmark '{}'".format(urlInAddRoute))
         app.logger.debug('stored url: ' + urlInAddRoute)
         app.logger.debug('bookmarks: {}'.format(bookmarks))
         return redirect(url_for('index'))
