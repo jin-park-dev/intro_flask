7f242d57a704ebdbac6a5c940d728bbacc52827e
Jin <jinparkhan+git@googlemail.com>
2017-01-01 14:38:55 +0000

    Added exercise files to ignore list

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,28 +1,41 @@
-from flask import Flask, render_template, url_for
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for
+
+from logging import DEBUG
 
 app = Flask(__name__)
+app.logger.setLevel(DEBUG)
 
 class User:
     def __init__(self, firstname, lastname):
         self.firstname = firstname
         self.lastname = lastname
-
     def initals(self):
         return "{}. {}.".format(self.firstname[0], self.lastname[0])
 
-    """Muahahah after learning from Mark Lutz book I'm comfortable doing this and understand!"""
-    # def __str__(self):
-    #     return "{} {}".format(self.firstname, self.lastname)
-    # def __repr__(self):
-    #     return "{} {}".format(self.firstname, self.lastname)
+bookmarks = []
+
+def store_bookmark(urlfromAddRoute):
+    bookmarks.append(dict(
+        url = urlfromAddRoute,
+        user = "reindert",
+        date = datetime.utcnow()
+    ))
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title="Title passed from view to template", user=User("Jin", "Park"))
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add():
+    if request.method == "POST":
+        urlInAddRoute = request.form['urlInAddHtml']
+        store_bookmark(urlInAddRoute)
+        app.logger.debug('stored url: ' + urlInAddRoute)
+        app.logger.debug('bookmarks: {}'.format(bookmarks))
+        return redirect(url_for('index'))
     return render_template('add.html')
 
 @app.errorhandler(404)
